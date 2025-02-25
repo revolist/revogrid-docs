87e1e1dbe7a0bb1557660338c7933908206e1389
maks <mojpo4tovik@gmail.com>
2025-02-25 12:16:08 +0000

    docs: event manager explained

--- pro/features.pro.ts
+++ pro/features.pro.ts
@@ -1,5 +1,5 @@
 export const featuresPro = [
-    { 
+    {
         title: 'Pivot Table',
         thumbnail: '/pivot.svg',
         videoUrl: '/video/pivot.mp4',
@@ -7,15 +7,15 @@ export const featuresPro = [
         description: `
         Transform multidimensional data into insightful visualizations.
         <ul>
-        <li>
-        <strong>Dynamic Pivoting</strong>: Seamlessly switch between a standard grid and pivot mode to analyze your data from different angles.
-        </li>
-        <li>
-        <strong>Custom Aggregation</strong>: Apply built-in or custom aggregation functions to summarize your data effectively and gain deeper insights.
-        </li>
-        <li>
-        <strong>Interactive Field Management</strong>: Drag and drop dimensions, rows, columns, and values with our intuitive configurator to customize your pivot table.
-        </li>
+            <li>
+                <strong>Dynamic Pivoting</strong>: Standard grid and pivot mode to analyze your data from different angles.
+            </li>
+            <li>
+                <strong>Custom Aggregation</strong>: Apply built-in or custom aggregation functions to summarize your data effectively and gain deeper insights.
+            </li>
+            <li>
+                <strong>Configuration Management</strong>: Drag and drop dimensions, rows, columns, and values to customize your pivot table.
+            </li>
         </ul>
         
         <strong>Perfect for</strong>: Applications requiring complex data analysis, where flexibility and control are crucial. Pivot Table feature ensures high performance, even with large datasets, making it an ideal solution for data-intensive applications.
@@ -25,60 +25,69 @@ export const featuresPro = [
         title: 'Smart Auto Fill',
         thumbnail: '/autofill.svg',
         videoUrl: '/video/autofill.mp4',
-        description: 'Automatically populating cells with a series, pattern based on the initial input. Perfect for repetitive tasks, saving users time.',
+        description:
+            'Automatically populating cells with a series, pattern based on the initial input. Perfect for repetitive tasks, saving users time.',
     },
     {
         title: 'Dynamic Cell Formats',
         thumbnail: '/renders.svg',
         videoUrl: '/video/renders.mp4',
-        description: 'Empower your data grid with visually rich, interactive, and context-driven cells. From ratings to timelines, our custom cell renders elevate data visualization to the next level.',
+        description:
+            'Empower your data grid with visually rich, interactive, and context-driven cells. From ratings to timelines, our custom cell renders elevate data visualization to the next level.',
     },
     {
-        "title": "Heat&Cold Maps",
-        "thumbnail": "/heatmap.svg",
-        "videoUrl": "/video/heatmap.mp4",
-        "description": "Data interpretation with a gradient legend, displaying the value range for heatmaps and coldmaps. Perfect for giving users context on how colors map to values."
+        title: 'Heat&Cold Maps',
+        thumbnail: '/heatmap.svg',
+        videoUrl: '/video/heatmap.mp4',
+        description:
+            'Data interpretation with a gradient legend, displaying the value range for heatmaps and coldmaps. Perfect for giving users context on how colors map to values.',
     },
     {
         title: 'Conditional Formatting',
         thumbnail: '/conditional-formatting.svg',
-        description: 'Customize cell styles based on specific conditions. Highlight important data, create visual cues, and improve the readability.',
+        description:
+            'Customize cell styles based on specific conditions. Highlight important data, create visual cues, and improve the readability.',
         videoUrl: '/video/conditional-formatting.mp4',
     },
     {
         title: 'Cell Validation',
         thumbnail: '/data-validation.svg',
-        description: 'Built-in data validation to ensure data integrity. Define customizable validation rules and error handling to maintain high-quality data.',
+        description:
+            'Built-in data validation to ensure data integrity. Define customizable validation rules and error handling to maintain high-quality data.',
         videoUrl: '/video/data-validation.mp4',
     },
     {
         title: 'Input Validation',
         thumbnail: '/data-validation-input.svg',
-        description: 'Advanced validation with plugin support to prevent invalid edits and highlight erroneous cells, ensuring data accuracy.',
+        description:
+            'Advanced validation with plugin support to prevent invalid edits and highlight erroneous cells, ensuring data accuracy.',
         videoUrl: '/video/data-validation-input.mp4',
     },
     {
         title: 'Pagination',
         thumbnail: '/pagination.svg',
-        description: 'Break larger datasets into smaller pages, enhancing both performance and user experience.',
+        description:
+            'Break larger datasets into smaller pages, enhancing both performance and user experience.',
         videoUrl: '/video/pagination.mp4',
     },
     {
         title: 'Formula',
         thumbnail: '/formula.svg',
         videoUrl: '/video/formula.mp4',
-        description: 'Complex formulas similar to Excel. Provides dynamic calculations & data manipulation.',  
+        description:
+            'Complex formulas similar to Excel. Provides dynamic calculations & data manipulation.',
     },
     {
         title: 'History',
         thumbnail: '/history.svg',
         videoUrl: '/video/history.mp4',
-        description: 'Track your history of changes. Undo and redo operations.',  
+        description: 'Track your history of changes. Undo and redo operations.',
     },
     {
         title: 'Cell Flash',
         thumbnail: '/cell-flash.svg',
-        description: 'Enable flashing cells to highlight updates, making it easy for users to spot changes in values.',
+        description:
+            'Enable flashing cells to highlight updates, making it easy for users to spot changes in values.',
         videoUrl: '/video/cell-flash.mp4',
     },
     {
@@ -90,49 +99,57 @@ export const featuresPro = [
     {
         title: 'Reference Data',
         thumbnail: '/reference-data.svg',
-        description: 'Manage static key/value pairs for displaying meaningful labels alongside coded values.',
+        description:
+            'Manage static key/value pairs for displaying meaningful labels alongside coded values.',
         videoUrl: '/video/reference-data.mp4',
     },
     {
         title: 'Tree Data View',
         thumbnail: '/tree-data.svg',
-        description: 'Display hierarchical data in a tree-like structure, enabling navigation and organization.',
+        description:
+            'Display hierarchical data in a tree-like structure, enabling navigation and organization.',
         videoUrl: '/video/tree-data.mp4',
     },
     {
         title: 'Master Detail',
         thumbnail: '/master-detail.svg',
         videoUrl: '/video/master-detail.mp4',
-        description: 'Expand rows to  reveal detailed child information. Display additional data or build customizable subtables within the grid.',     
+        description:
+            'Expand rows to  reveal detailed child information. Display additional data or build customizable subtables within the grid.',
     },
     {
         title: 'Row Grouping Drag and Drop',
         thumbnail: '/grouping.svg',
-        description: 'Drag columns to the top panel to create grouped rows, improving data organization and visibility.',
+        description:
+            'Drag columns to the top panel to create grouped rows, improving data organization and visibility.',
         videoUrl: '/video/grouping.mp4',
     },
     {
         thumbnail: '/row-transpose.svg',
         title: 'Row Transpose',
-        description: 'Modify the grid by transposing its data: columns become rows and vice versa.',
+        description:
+            'Modify the grid by transposing its data: columns become rows and vice versa.',
         videoUrl: '/video/row-transpose.mp4',
     },
     {
         title: 'Row Odd Styling',
         thumbnail: '/odd-rows.svg',
-        description: 'Apply distinct styles to odd rows for improved readability and data distinction.',
+        description:
+            'Apply distinct styles to odd rows for improved readability and data distinction.',
         videoUrl: '/video/row-odd.mp4',
     },
     {
         title: 'Row Custom Heading',
         thumbnail: '/row-heading.svg',
-        description: 'Add custom heading to rows for improved readability and data distinction.',
+        description:
+            'Add custom heading to rows for improved readability and data distinction.',
         videoUrl: '/video/row-heading.mp4',
     },
     {
         title: 'Row Advanced Drag and Drop',
         thumbnail: '/drag-and-drop.svg',
-        description: "Sophisticated drag-and-drop functionality with custom behavior and interactions, enhancing the grid's interactivity and usability.",
+        description:
+            "Sophisticated drag-and-drop functionality with custom behavior and interactions, enhancing the grid's interactivity and usability.",
         videoUrl: '/video/drag-and-drop.mp4',
     },
     {
@@ -144,49 +161,58 @@ export const featuresPro = [
     {
         title: 'Column Stretch',
         thumbnail: '/column-stretch.svg',
-        description: 'Automatically adjusts column widths to fit the grid layout efficiently.',
+        description:
+            'Automatically adjusts column widths to fit the grid layout efficiently.',
         videoUrl: '/video/column-stretch.mp4',
     },
     {
         title: 'Column Selection',
         thumbnail: '/column-selection.svg',
-        description: 'Enable easy selection of entire columns for enhanced data manipulation.',
+        description:
+            'Enable easy selection of entire columns for enhanced data manipulation.',
         videoUrl: '/video/column-selection.mp4',
     },
     {
         title: 'Next Line Focus (WCAG)',
         thumbnail: '/auto-focus.svg',
-        description: 'Automatically move focus to the next row for streamlined data entry and editing.',
+        description:
+            'Automatically move focus to the next row for streamlined data entry and editing.',
         videoUrl: '/video/auto-focus.mp4',
     },
     {
         title: 'Clipboard with JSON and Advanced Objects',
         thumbnail: '/clipboard.svg',
-        description: 'Support for JSON and other objects. Copy and paste complex data structures explained.',
+        description:
+            'Support for JSON and other objects. Copy and paste complex data structures explained.',
         videoUrl: '/video/clipboard.mp4',
     },
     {
         title: 'Context Menu',
         thumbnail: '/menu.svg',
-        description: 'Customize context menu options. Add custom actions to the grid.',
+        description:
+            'Customize context menu options. Add custom actions to the grid.',
         videoUrl: '/video/row-heading.mp4',
     },
     {
         title: 'Excel Export',
         thumbnail: '/export.svg',
-        description: 'Export data to Excel format. Supported types include `xlsx`, `xlsm`, `xlsb`, `xls`, and more.',
+        description:
+            'Export data to Excel format. Supported types include `xlsx`, `xlsm`, `xlsb`, `xls`, and more.',
         videoUrl: '/video/excel-export.mp4',
     },
     {
         title: 'Event Manager',
         thumbnail: '/event-manager.svg',
-        description: 'Handles, customizes, and optimizes events. Creates complex interactions and workflows.',
+        description:
+            'Handles, customizes, and optimizes events. Use one event to process multiple actions.',
         sponsor: '/guide/event-manager',
+        videoUrl: '/video/event-manager.mp4',
     },
     {
         title: 'Multi-Filtering',
         thumbnail: '/filtering.svg',
-        description: 'Multi-condition filters logic example to refine and display your data more effectively.',
+        description:
+            'Multi-condition filters logic example to refine and display your data more effectively.',
         videoUrl: '/video/filter-multi.mp4',
     },
     {
@@ -198,13 +224,15 @@ export const featuresPro = [
     {
         title: 'Advanced Slider Filtering',
         thumbnail: '/filtering.svg',
-        description: 'Advanced slider filter, defining a range of values for filtering.',
+        description:
+            'Advanced slider filter, defining a range of values for filtering.',
         videoUrl: '/video/filter-advance-slider.mp4',
     },
     {
         title: 'Header Input Filtering',
         thumbnail: '/filtering.svg',
-        description: 'Header input filter, filtering data based on user input in the first row.',
+        description:
+            'Header input filter, filtering data based on user input in the first row.',
         videoUrl: '/video/filter-header.mp4',
     },
     {
@@ -216,6 +244,7 @@ export const featuresPro = [
     {
         title: 'Overlay layers',
         thumbnail: '/overlay.svg',
-        description: 'Push and pop layers on top of the grid to enhance the user experience.',
+        description:
+            'Push and pop layers on top of the grid to enhance the user experience.',
     },
 ]
